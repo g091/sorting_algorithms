3c1ff828fb008b4d48ac49ace6c64178b7cfb8d7
g091 <gotieno791@gmail.com>
2022-07-24 01:45:05 +0300

    added swap

--- sort.h
+++ sort.h
@@ -22,7 +22,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-void swap(listint_t *head, listint_t *next);
+void swap(listint_t **head, listint_t *node1, listint_t *node2);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
